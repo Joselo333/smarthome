9da639676fe3bee30d37d423fb7a510301c6bd5c
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-25 00:09:41 -0400

    Se agrega boton de recuperar

--- vistas/gui/recupero_clave.php
+++ vistas/gui/recupero_clave.php
@@ -6,19 +6,26 @@
         Recuperar contraseña
       </p>
     </div>
-    <div class="row my-5">
+    <div class="row my-4">
       <div class="col-3"></div>
       <div class="col-6" id="form0">
-        <div class="form-floating mb-3">
-          <input type="number" class="form-control" id="input-rut" placeholder="12345678-9" required>
-          <label for="input-rut">RUT</label>
-        </div>
-        <div class="form-floating mt-3">
-          <input type="email" class="form-control" id="input-email" placeholder="correo@mail.com" required>
-          <label for="input-email">Email</label>
-        </div>
+        <form>
+          <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="input-rut" placeholder="12345678-9" required>
+            <label for="input-rut">RUT</label>
+          </div>
+          <div class="form-floating mt-3">
+            <input type="email" class="form-control" id="input-email" placeholder="correo@mail.com" required>
+            <label for="input-email">Email</label>
+          </div>
+        </form>
       </div>
       <div class="col-3"></div>
+      <div class="row text-center">
+        <a href="recupero_clave.php">
+          <button type="button" class="btn btn-primary my-3">Recuperar</button>
+        </a>
+      </div>
     </div>
   </div>
 <?php require('../layout/footer.php') ?>
