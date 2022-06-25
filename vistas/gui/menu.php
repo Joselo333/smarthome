97941f7dfac301ed59c325932cf38ca0ab3550e4
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-25 18:28:54 -0400

    Se inicializa el menu.php y se corrigen cosas pequeñas en recupero_clave.php

---
+++ vistas/gui/menu.php
@@ -0,0 +1,4 @@
+<?php require('../layout/head.php') ?>
+<?php require('../layout/slider.php') ?>
+  <link rel="stylesheet" href="../estilos/recupero_clave.css">
+    <h1>Menú</h1><?php require('../layout/footer.php') ?>
--- vistas/gui/recupero_clave.php
+++ vistas/gui/recupero_clave.php
@@ -1,4 +1,5 @@
 <?php require('../layout/head.php') ?>
+<?php require('../layout/slider.php') ?>
   <link rel="stylesheet" href="../estilos/recupero_clave.css">
   <div class="container">
     <div class="row">
@@ -14,14 +15,14 @@
             <input type="number" class="form-control" id="input-rut" placeholder="12345678-9" required>
             <label for="input-rut">RUT</label>
           </div>
-          <div class="form-floating mt-3">
+          <div class="form-floating mb-3">
             <input type="email" class="form-control" id="input-email" placeholder="correo@mail.com" required>
             <label for="input-email">Email</label>
           </div>
         </form>
       </div>
       <div class="col-3"></div>
-      <div class="row text-center">
+      <div class="row text-center my-5">
         <a href="recupero_clave.php">
           <button type="button" class="btn btn-primary my-3">Recuperar</button>
         </a>
