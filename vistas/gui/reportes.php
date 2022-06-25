e9e58bf0d5c3c677bd7286220e4e2140c3084118
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-25 19:08:41 -0400

    Se comenta la instruccion de trabajo.

--- vistas/gui/reportes.php
+++ vistas/gui/reportes.php
@@ -1,4 +1,6 @@
 <?php require('../layout/head.php') ?>
 <?php require('../layout/slider.php') ?>
-
+<!-- 
+  AGREGUEN EL CODIGO CORRESPONDIENTE AL CUERPO DE LA PAG.
+-->
 <?php require('../layout/footer.php') ?>
