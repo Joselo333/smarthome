332481bf241238f34849232b4b383a4d56cff720
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-24 23:11:39 -0400

    Se agregan las imagenes de los logos de las intituciones y he creado tanto el footer como el header inicial

---
+++ vistas/gui/recupero_clave.php
@@ -0,0 +1,5 @@
+<?php require('../layout/head.php') ?>
+  <div class="container text-center">
+    <h1>hola</h1>
+</div>
+<?php require('../layout/footer.php') ?>
---
+++ vistas/layout/footer.php
@@ -0,0 +1,22 @@
+
+    <footer class="container">
+        <div class="row text-center border m-2 p-1">
+            <div class="col-1">
+                <a href="/"><img class="img-fluid" src="../../imagenes/Logo_UDA.png" alt="" width=16px heigth=16px></a>
+            </div>
+            <div class="col-1">
+                <a href="/"><i class="bi bi-facebook"></i></a>
+            </div>
+            <div class="col-8">
+                <h6>UDA derechos reservados ©</h6>
+            </div>
+            <div class="col-1">
+                <a href="/"><i class="bi bi-instagram"></i></a>
+            </div>
+            <div class="col-1">
+                <a href="/"><i class="bi bi-twitter"></i></a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
