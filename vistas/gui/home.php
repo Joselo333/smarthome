091ef8e3c2191038b33436a0c8035ccd456a080d
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-25 19:01:24 -0400

    Se agrega header, slider y footer a todas las paginas.

---
+++ vistas/gui/home.php
@@ -0,0 +1,4 @@
+<?php require('../layout/head.php') ?>
+<?php require('../layout/slider.php') ?>
+
+<?php require('../layout/footer.php') ?>
--- vistas/layout/head.php
+++ vistas/layout/head.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../estilos/head.css">
   </head>
   <body>
     <header class="text-center container">
@@ -29,15 +30,15 @@
           <img class="img-fluid" src="../../imagenes/LOGO_UTEM.png" alt="Logo de la UTEM">
         </div>
       </div>
-      <div class="row">
-        <div class="col-4"></div>
+      <div class="row ">
+        <div class="col-3"></div>
         <div class="col-6">
           <ul class="nav nav-pills position-relative top-50 start-50 translate-middle">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="../gui/home.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">opcion1</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">opcion2</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">opcion3</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">opcion4</a></li>
           </ul>
         </div>
       </div>
