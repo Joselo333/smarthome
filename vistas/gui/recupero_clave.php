d02a5e9c8352c11b53cfb235204e8522f7ccd18a
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-25 00:02:47 -0400

    Se crea los input del formulario de recuperar clave

--- vistas/gui/recupero_clave.php
+++ vistas/gui/recupero_clave.php
@@ -6,5 +6,19 @@
         Recuperar contraseña
       </p>
     </div>
+    <div class="row my-5">
+      <div class="col-3"></div>
+      <div class="col-6" id="form0">
+        <div class="form-floating mb-3">
+          <input type="number" class="form-control" id="input-rut" placeholder="12345678-9" required>
+          <label for="input-rut">RUT</label>
+        </div>
+        <div class="form-floating mt-3">
+          <input type="email" class="form-control" id="input-email" placeholder="correo@mail.com" required>
+          <label for="input-email">Email</label>
+        </div>
+      </div>
+      <div class="col-3"></div>
+    </div>
   </div>
 <?php require('../layout/footer.php') ?>
