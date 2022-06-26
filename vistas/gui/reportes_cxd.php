614fd496f91965b7ed44cc66ac19d49332bdb6a9
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-26 04:03:17 -0400

    se inicia la pantalla de reportes

---
+++ vistas/gui/reportes_cxd.php
@@ -0,0 +1,33 @@
+<?php require('../layout/head.php') ?>
+<link rel="stylesheet" href="../estilos/paginas_generales.css">
+<div class="container">
+  <div class="row">
+    <p class="col-12 texto-titulo">
+      Reportes
+    </p>
+  </div>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-9">
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a class="nav-link bg-secondary" aria-current="page" href="reportes_cxt.php">Consumo por tiempo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="reportes_cxd.php">Consumo por dispositivo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link bg-secondary" href="reportes_cxf.php">Consumo por fuente</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="row caja-graficos">
+    <div class="col-2"></div>
+    <div class="col-8">
+      
+    </div>
+    <div class="col-2"></div>
+  </div>
+</div>
+<?php require('../layout/footer.php') ?>
--- vistas/layout/footer.php
+++ vistas/layout/footer.php
@@ -1,8 +1,8 @@
 
     <footer class="footer">
-        <div class="row text-center p-5 ">
+        <div class="row">
             <div class="col-1">
-                <a href="https://uda.cl/"><img class="img-fluid" src="../../imagenes/Logo_UDA.png" alt="" width=16px heigth=16px></a>
+                <a href="https://uda.cl/" target="_blank"><img class="img-fluid" src="../../imagenes/header/logo-uda.png" alt="" width=16px heigth=16px></a>
             </div>
             <div class="col-1">
                 <a href="#"><i class="bi bi-facebook"></i></a>
