e661867b200f41bb9625fd6d62849bdece9a839b
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-25 21:41:32 -0400

    se mejora footer

--- vistas/layout/footer.php
+++ vistas/layout/footer.php
@@ -1,20 +1,20 @@
 
-    <footer class="bg-light">
-        <div class="row text-center m-2 p-1 ">
+    <footer class="footer">
+        <div class="row text-center p-5 ">
             <div class="col-1">
-                <a href="/"><img class="img-fluid" src="../../imagenes/Logo_UDA.png" alt="" width=16px heigth=16px></a>
+                <a href="https://uda.cl/"><img class="img-fluid" src="../../imagenes/Logo_UDA.png" alt="" width=16px heigth=16px></a>
             </div>
             <div class="col-1">
-                <a href="/"><i class="bi bi-facebook"></i></a>
+                <a href="#"><i class="bi bi-facebook"></i></a>
             </div>
             <div class="col-8">
-                <h6>UDA derechos reservados ©</h6>
+                <p class="footer-body">UDA derechos reservados ©</p>
             </div>
             <div class="col-1">
-                <a href="/"><i class="bi bi-instagram"></i></a>
+                <a href="#"><i class="bi bi-instagram"></i></a>
             </div>
             <div class="col-1">
-                <a href="/"><i class="bi bi-twitter"></i></a>
+                <a href="#"><i class="bi bi-twitter"></i></a>
             </div>
         </div>
     </footer>
