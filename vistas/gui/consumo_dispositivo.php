73995367608c3429a01523b36f65c0c4f7d226f7
Hector Torres <hectortorres770@gmail.com>
2022-06-27 00:44:11 -0400

    Update consumo_dispositivo.php
    Avance en consumo_dispositivo

--- vistas/gui/consumo_dispositivo.php
+++ vistas/gui/consumo_dispositivo.php
@@ -1,6 +1,43 @@
 <?php require('../layout/head.php') ?>
 <?php require('../layout/slider.php') ?>
-<!-- 
-  AGREGUEN EL CODIGO CORRESPONDIENTE AL CUERPO DE LA PAG.
--->
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h2 class="text-center">Gestionar Dispositivos<h2>
+					<div class="text-center">
+						<button type="button" class="btn btn-secondary">Filtrar</button>
+						<button type="button" class="btn btn-secondary">Agregar</button>
+						<button type="button" class="btn btn-secondary">Editar</button>
+						<button type="button" class="btn btn-secondary">Eliminar</button>
+					</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+		</div>
+
+		<div class="col-md-4">
+			<div class="input-group w-100">
+				<input type="text" class="form-control text-center" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+				<button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row">
+
+		
+	</div>
+
+</div>
+
+
+
+
+
+
+<h2></h2>
+
 <?php require('../layout/footer.php') ?>
