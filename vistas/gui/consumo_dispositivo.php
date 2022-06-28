9ee56305e815b3f039a4a805324c39a3c07526dc
Hector Torres <hectortorres770@gmail.com>
2022-06-28 00:05:41 -0400

    Avance en consumo dispositivo y agregué la carpeta de íconos

--- vistas/gui/consumo_dispositivo.php
+++ vistas/gui/consumo_dispositivo.php
@@ -11,11 +11,20 @@
 		<div class="col-10 text-center">
 			<div class="card p-4 my-3">
 				<div class="row">
-					
+
 					<div class="col-sm-4">
 						<div class="card">
-							<div class="card-body">
-								<h5 class="card-title">Special title treatment</h5>
+							<div class="card-body ">
+								<div class="row">
+								<div class="col-sm-3"></div>
+									<div class="col-sm-6">
+										<h3 class="card-title">Tele1 [LG]</h3>
+
+									</div>
+								<div class="col-sm-3"><i ><img class="img-fluid" src="../../imagenes/icons/refrigerator.png" alt="Logo de la UDA"></i></div>
+								
+
+								</div>
 								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
 								<a href="#" class="btn btn-primary">Go somewhere</a>
 							</div>
