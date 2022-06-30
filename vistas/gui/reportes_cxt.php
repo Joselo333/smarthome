79de140774dbf4a983bc457291a35648592be3b3
JoseJorquera <josejorquerapallauta@gmail.com>
2022-06-29 22:57:17 -0400

    He creado la carpeta que almacena la imagenes de los graficos, y cree la esructura inicial de la pagina de reportes

--- vistas/gui/reportes_cxt.php
+++ vistas/gui/reportes_cxt.php
@@ -22,5 +22,19 @@
       </ul>
     </div>
   </div>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-8 caja-graficos">
+    <select class="form-select filtro-tiempo" aria-label="Default select example">
+      <option>Selecciona tiempo</option>
+      <option value="1" selected>Hora</option>
+      <option value="2">Dia</option>
+      <option value="3">Mes</option>
+    </select>
+      <img class="img-fluid" src="../../imagenes/reportes/grafico_consumo_hora.png" alt="Grafico de el consumo por tiempo">
+    </div>
+    <div class="col-2"></div>
+  </div>
+</div>
 </div>
 <?php require('../layout/footer.php') ?>
