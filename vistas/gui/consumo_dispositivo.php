2d75c662addd133aca23bf310cc5a1ceb6feb523
Hector Torres <hectortorres770@gmail.com>
2022-07-01 00:45:52 -0400

    Avance en consumo_dispositivo

--- vistas/gui/consumo_dispositivo.php
+++ vistas/gui/consumo_dispositivo.php
@@ -1,5 +1,6 @@
 <?php require('../layout/head.php') ?>
 <link rel="stylesheet" href="../estilos/paginas_generales.css">
+<link rel="stylesheet" href="../estilos/iconos_dispositivos.css">
 
 <container class="container mt-4">
 	<div class="row">
@@ -11,51 +12,92 @@
 		<div class="col-10 text-center">
 			<div class="card p-4 my-3">
 				<div class="row">
+					<div class="col-sm-4">
+						<div class="card">
+							<div class="card-body ">
+								<div class="row">
+									<div class="col-sm-3"></div>
+									<div class="col-sm-6">
+										<h3 class="card-title">Tele1 [LG]</h3>
+									</div>
+									<div class="col-sm-3 iconsize"><i><img class="img-fluid" src="../../imagenes/icons/tv.png" alt="Logo de la UDA"></i></div>
+								</div>
+								<br>
+								<div class="row">
+									<div class="col-sm-6">
+										<h3 class="card-text">500 kWh</h3>
+									</div>
+									<div class="col-sm-3">
+									</div>
+
+									<div class="col-sm-3">
+										<a href="#" class="btn btn-outline-secondary iconsize-button "><i><img class="img-fluid" src="../../imagenes/icons/bars-chart-ascending.png" alt="Logo de la UDA"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 
 					<div class="col-sm-4">
 						<div class="card">
 							<div class="card-body ">
 								<div class="row">
-								<div class="col-sm-3"></div>
+									<div class="col-sm-3"></div>
 									<div class="col-sm-6">
-										<h3 class="card-title">Tele1 [LG]</h3>
-
+										<h3 class="card-title">Lavadora [Mademsa]</h3>
 									</div>
-								<div class="col-sm-3"><i ><img class="img-fluid" src="../../imagenes/icons/refrigerator.png" alt="Logo de la UDA"></i></div>
-								
-
+									<div class="col-sm-3 iconsize"><i><img class="img-fluid" src="../../imagenes/icons/washing-machine.png" alt="Logo de la UDA"></i></div>
 								</div>
-								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
+								<br>
+
+								<div class="row">
+									<div class="col-sm-6">
+										<h3 class="card-text">500 kWh</h3>
+									</div>
+									<div class="col-sm-3">
+									</div>
+
+									<div class="col-sm-3">
+										<a href="#" class="btn btn-outline-secondary iconsize-button "><i><img class="img-fluid" src="../../imagenes/icons/bars-chart-ascending.png" alt="Logo de la UDA"></i></a>
+									</div>
+								</div>
+
+
 							</div>
 						</div>
 					</div>
 
 					<div class="col-sm-4">
 						<div class="card">
-							<div class="card-body">
-								<h5 class="card-title">Special title treatment</h5>
-								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
-							</div>
-						</div>
-					</div>
+							<div class="card-body ">
+								<div class="row">
+									<div class="col-sm-3"></div>
+									<div class="col-sm-6">
+										<h3 class="card-title">Impresora [HP]</h3>
+									</div>
+									<div class="col-sm-3 iconsize"><i><img class="img-fluid" src="../../imagenes/icons/print.png" alt="Logo de la UDA"></i></div>
+									<br>
+									<div class="row">
+										<div class="col-sm-6">
+											<h3 class="card-text">500 kWh</h3>
+										</div>
+										<div class="col-sm-3">
+										</div>
 
-					<div class="col-sm-4">
-						<div class="card">
-							<div class="card-body">
-								<h5 class="card-title">Special title treatment</h5>
-								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
+										<div class="col-sm-3">
+											<a href="#" class="btn btn-outline-secondary iconsize-button "><i><img class="img-fluid" src="../../imagenes/icons/bars-chart-ascending.png" alt="Logo de la UDA"></i></a>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
+
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-1"></div>
+			<div class="col-1"></div>
 
-	</div>
+		</div>
 
 
 </container>
