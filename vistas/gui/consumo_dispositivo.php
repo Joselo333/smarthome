71ded2ac42a982484c592c6fb7722a7befa6ef77
Hector Torres <hectortorres770@gmail.com>
2022-07-01 23:24:52 -0400

    consumo_dispositivos terminado

--- vistas/gui/consumo_dispositivo.php
+++ vistas/gui/consumo_dispositivo.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="../estilos/iconos_dispositivos.css">
 
 <container class="container mt-4">
+
 	<div class="row">
 		<h2 class="text-center texto-titulo">Consumo por Dispositivo<h2>
 	</div>
@@ -12,93 +13,118 @@
 		<div class="col-10 text-center">
 			<div class="card p-4 my-3">
 				<div class="row">
+					<!-- Card 1 -->
 					<div class="col-sm-4">
 						<div class="card">
 							<div class="card-body ">
+								<!-- 3 col -->
 								<div class="row">
-									<div class="col-sm-3"></div>
-									<div class="col-sm-6">
-										<h3 class="card-title">Tele1 [LG]</h3>
+									<div class="col-5">
+										<h3 class="card-title mt-2">[LG]</h3>
 									</div>
-									<div class="col-sm-3 iconsize"><i><img class="img-fluid" src="../../imagenes/icons/tv.png" alt="Logo de la UDA"></i></div>
+									<div class="col-4">
+										<h3 class="card-title mt-2 mr-4">Tele1</h3>
+									</div>
+									<div class="col-3">
+										<i class=""><img id="iconos" class="img-fluid" width="50px" src="../../imagenes/icons/tv.png" alt="Logo de la UDA"></i>
+									</div>
+
 								</div>
 								<br>
+								<br>
+								<!-- 2 col -->
 								<div class="row">
 									<div class="col-sm-6">
-										<h3 class="card-text">500 kWh</h3>
+										<h3 class="card-text text-muted mt-2">500 kWh</h3>
 									</div>
 									<div class="col-sm-3">
 									</div>
 
 									<div class="col-sm-3">
-										<a href="#" class="btn btn-outline-secondary iconsize-button "><i><img class="img-fluid" src="../../imagenes/icons/bars-chart-ascending.png" alt="Logo de la UDA"></i></a>
+										<a class="enlaces btn btn-outline-secondary" href="consumo_dispositivo.php"><img class="img-fluid" width="50px" src="../../imagenes/icons/bars-chart-ascending.png" alt=""></a>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
+					<!-- Card 2 -->
 					<div class="col-sm-4">
 						<div class="card">
 							<div class="card-body ">
+								<!-- 3 col -->
 								<div class="row">
-									<div class="col-sm-3"></div>
-									<div class="col-sm-6">
-										<h3 class="card-title">Lavadora [Mademsa]</h3>
+									<div class="col-5">
+										<h3 class="card-title mt-2">[Mademsa]</h3>
 									</div>
-									<div class="col-sm-3 iconsize"><i><img class="img-fluid" src="../../imagenes/icons/washing-machine.png" alt="Logo de la UDA"></i></div>
+									<div class="col-4">
+										<h3 class="card-title mt-2 mr-4">Lavadora</h3>
+									</div>
+									<div class="col-3">
+										<i class=""><img id="iconos" class="img-fluid" width="50px" src="../../imagenes/icons/refrigerator.png" alt="Logo de la UDA"></i>
+									</div>
+
+								</div>
+
+								<br>
+								<br>
+								<!-- 2 col -->
+								<div class="row">
+									<div class="col-sm-6">
+										<h3 class="card-text text-muted mt-2">500 kWh</h3>
+									</div>
+									<div class="col-sm-3">
+									</div>
+
+									<div class="col-sm-3">
+										<a class="enlaces btn btn-outline-secondary" href="consumo_dispositivo.php"><img class="img-fluid" width="50px" src="../../imagenes/icons/bars-chart-ascending.png" alt=""></a>
+									</div>
+								</div>
+
+
+							</div>
+						</div>
+					</div>
+					<!-- Card 3 -->
+					<div class="col-sm-4">
+						<div class="card">
+							<div class="card-body ">
+								<!-- 3 col -->
+								<div class="row">
+									<div class="col-5">
+										<h3 class="card-title mt-2">[HP]</h3>
+									</div>
+									<div class="col-4">
+										<h3 class="card-title mt-2 mr-4">Impresora</h3>
+									</div>
+									<div class="col-3">
+										<i class=""><img id="iconos" class="img-fluid" width="50px" src="../../imagenes/icons/print.png" alt="Logo de la UDA"></i>
+									</div>
+
 								</div>
 								<br>
-
+								<br>
+								<!-- 2 col -->
 								<div class="row">
 									<div class="col-sm-6">
-										<h3 class="card-text">500 kWh</h3>
+										<h3 class="card-text text-muted mt-2">500 kWh</h3>
 									</div>
 									<div class="col-sm-3">
 									</div>
 
 									<div class="col-sm-3">
-										<a href="#" class="btn btn-outline-secondary iconsize-button "><i><img class="img-fluid" src="../../imagenes/icons/bars-chart-ascending.png" alt="Logo de la UDA"></i></a>
-									</div>
-								</div>
-
-
-							</div>
-						</div>
-					</div>
-
-					<div class="col-sm-4">
-						<div class="card">
-							<div class="card-body ">
-								<div class="row">
-									<div class="col-sm-3"></div>
-									<div class="col-sm-6">
-										<h3 class="card-title">Impresora [HP]</h3>
-									</div>
-									<div class="col-sm-3 iconsize"><i><img class="img-fluid" src="../../imagenes/icons/print.png" alt="Logo de la UDA"></i></div>
-									<br>
-									<div class="row">
-										<div class="col-sm-6">
-											<h3 class="card-text">500 kWh</h3>
-										</div>
-										<div class="col-sm-3">
-										</div>
-
-										<div class="col-sm-3">
-											<a href="#" class="btn btn-outline-secondary iconsize-button "><i><img class="img-fluid" src="../../imagenes/icons/bars-chart-ascending.png" alt="Logo de la UDA"></i></a>
-										</div>
+										<a class="enlaces btn btn-outline-secondary" href="consumo_dispositivo.php"><img class="img-fluid" width="50px" src="../../imagenes/icons/bars-chart-ascending.png" alt=""></a>
 									</div>
 								</div>
 							</div>
 						</div>
-
 					</div>
+
 				</div>
 			</div>
-			<div class="col-1"></div>
-
 		</div>
+		<div class="col-1"></div>
 
+	</div>
 
 </container>
 
