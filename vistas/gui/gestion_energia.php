588a37f410bc9ee8c824ad909fd81c2da86e7052
Hector Torres <hectortorres770@gmail.com>
2022-07-03 01:22:38 -0400

    gestion_energía terminado

--- vistas/gui/gestion_energia.php
+++ vistas/gui/gestion_energia.php
@@ -3,50 +3,155 @@
 <link rel="stylesheet" href="../estilos/paginas_generales.css">
 
 <div class="container mt-4 mr-4">
-  <div class="col-1 text-center"></div>
-  <div class="col-10 text-center">
-    <div class="card p-4 my-3">
-      <!-- row 1 -->
-      <div class="row">
+	<div class="row">
+		<h2 class="text-center texto-titulo">Gestión de Energía<h2>
+	</div>
 
-        <div class="col-md-4">
+	<!-- Consumo General -->
+	<div class="row">
+		<div class="col-1"></div>
+		<div class="col-10 text-center">
+			<div class="card p-4 my-3">
+				<div class="row">
+					<!-- row 1 -->
+					<div class="col-md-4">
+						<h3 class="text-center texto-titulo">Consumo Total</h3>
+					</div>
 
-          <h3 class="text-center texto-titulo">Consumo Total</h3>
-        </div>
+					<div class="col-md-4">
+						<a class="enlaces btn btn-outline-secondary" href="consumo_dispositivo.php"><img class="img-fluid" width="38px" src="../../imagenes/icons/bars-chart-ascending.png" alt=""></a>
+					</div>
+					<div class="col-md-4">
+						<h3 class="text-center texto-titulo">Ganancia / Pérdida</h3>
+					</div>
+				</div>
+				<br>
+				<!--  -->
+				<div class="row">
+					<div class="col-md-3">
+						<p class="fs-6 text-success">Sumistro: 500kWh</p>
+					</div>
 
-        <div class="col-md-4">
-          <a class="enlaces btn btn-outline-secondary" href="consumo_dispositivo.php"><img class="img-fluid" width="38px" src="../../imagenes/icons/bars-chart-ascending.png" alt=""></a>
-        </div>
-        <div class="col-md-4">
+					<div class="col-md-3 text-success">
+						<p class="fs-6 text-danger">Consumo: 450kWh</p>
+					</div>
 
-          <h3 class="text-center texto-titulo">Ganancia / Pérdida</h3>
+					<div class="col-md-3">
+						<p class="fs-6 text-success">Ganancia: $200 / dia </p>
+					</div>
 
-        </div>
-      </div>
+					<div class="col-md-3 text-success">
+						<p class="fs-6 text-danger">Gasto: $200 / dia</p>
+					</div>
+				</div>
 
-      <br>
-      <!--  -->
-      <div class="row">
-        <div class="col-md-3">
-          <p class="fs-6 text-success">Sumistro: 500kWh</p>
-        </div>
+			</div>
 
-        <div class="col-md-3 text-success">
-          <p class="fs-6 text-danger">Consumo: 450kWh</p>
-        </div>
+		</div>
+	</div>
+	<br>
 
-        <div class="col-md-3">
-          <p class="fs-6 text-success">Ganancia: $200 / dia </p>
-        </div>
+	<!-- Consumo por tipo de suministro -->
+	<div class="row">
+		<div class="col-1"></div>
+		<div class="col-10 text-center">
 
-        <div class="col-md-3 text-success">
-          <p class="fs-6 text-danger">Gasto: $200 / dia</p>
-        </div>
-      </div>
+			<!-- Paneles Solares -->
+			<div class="card p-4 my-3">
+				<div class="row">
+					<div class="col-md-4">
+						<h3 class="text-center texto-titulo">Paneles Solares</h3>
+						<br>
+						<br>
+						<div class="row">
+							<div class="col-md-4">
+								<i class=""><img class="rounded-circle mt-2 border border-dark p-2" width="55px" src="../../imagenes/icons/sun.png" alt="Logo de la UDA"></i>
+							</div>
+							<div class="col-md-6">
+								<p class="fs-6 text-success">Sumistro: 500kWh</p>
+								<p class="fs-6 text-danger">Consumo: 450kWh</p>
+							</div>
+						</div>
 
+					</div>
+					<div class="col-md-4">
+						<p class="fs-4 mt-2">nombre_empresa</p>
+						<br>
+						<br>
+						<p class="fs-6 text-success">Ganancia: $200 / dia </p>
+						<p class="fs-6 text-danger">Gasto: $200 / dia</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid  border border-dark mt-2 p-1" width="220px" src="..\..\imagenes\gestion_energia\green-chart.png" alt="Logo de la UDA">
+					</div>
+				</div>
+			</div>
 
-    </div>
-  </div>
+			<!-- Suministro Eléctrico -->
+			<div class="card p-4 my-3">
+
+				<div class="row">
+					<div class="col-md-4">
+						<h3 class="text-center texto-titulo">Suministro Eléctrico</h3>
+						<br>
+						<br>
+						<div class="row">
+							<div class="col-md-4">
+								<i class=""><img class="rounded-circle mt-2 border border-dark p-2" width="55px" src="../../imagenes/icons/plug.png" alt="Logo de la UDA"></i>
+							</div>
+							<div class="col-md-6">
+								<p class="fs-6 text-success">Sumistro: 500kWh</p>
+								<p class="fs-6 text-danger">Consumo: 450kWh</p>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-4">
+						<p class="fs-4 mt-2">nombre_empresa</p>
+						<br>
+						<br>
+						<p class="fs-6 text-success">Ganancia: $200 / dia </p>
+						<p class="fs-6 text-danger">Gasto: $200 / dia</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid  border border-dark mt-2 p-1" width="220px" src="..\..\imagenes\gestion_energia\purple-chart.png" alt="Logo de la UDA">
+					</div>
+				</div>
+			</div>
+
+			<!-- Energía Eólica -->
+			<div class="card p-4 my-3">
+				<div class="row">
+					<div class="col-md-4">
+						<h3 class="text-center texto-titulo">Energía Eólica</h3>
+						<br>
+						<br>
+						<div class="row">
+							<div class="col-md-4">
+								<i class=""><img class="rounded-circle mt-2 border border-dark p-2" width="55px" src="../../imagenes/icons/wind.png" alt="Logo de la UDA"></i>
+							</div>
+							<div class="col-md-6">
+								<p class="fs-6 text-success">Sumistro: 500kWh</p>
+								<p class="fs-6 text-danger">Consumo: 450kWh</p>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-4">
+						<p class="fs-4 mt-2">nombre_empresa</p>
+						<br>
+						<br>
+						<p class="fs-6 text-success">Ganancia: $200 / dia </p>
+						<p class="fs-6 text-danger">Gasto: $200 / dia</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid  border border-dark mt-2 p-1" width="220px" src="..\..\imagenes\gestion_energia\yellow-chart.png" alt="Logo de la UDA">
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
 </div>
 
 </div>
